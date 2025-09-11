bab3e1a8eeabeebb310ded74fbca47530129ed4f
Ruhul Amin <ruhulthisis@gmail.com>
2025-09-12 05:52:41 +0600

    Nested Loop

---
+++ chapter_four/nestedloop.js
@@ -0,0 +1,20 @@
+
+
+// var n = 5
+// for(let i = 1; i <= n; i++){
+//     var result = ''
+//    for( let j =1 ; j<= i; j++){
+//     result += j + ' '
+//    }
+//    console.log(result);
+// }
+
+var n = 5;
+
+for( i = 1; i <= n; i++){
+    result =''
+    for (var j =1; j<=n ; j++){
+     result += "*" + ' '
+    }
+    console.log(result);
+}
