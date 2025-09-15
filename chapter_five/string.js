81f98200a7c83fb1fd73a1cbb82ee9176873b8a4
Ruhul Amin <ruhulthisis@gmail.com>
2025-09-16 02:01:52 +0600

    learning strin method

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -23,7 +23,9 @@
 */
 
 
-// Escape Notation 
+// Escape Notation //
+
+
 // var str = 'hello 'world'' ; ai khane 'world text unwanted js ar syntax ar birudhe chole jasse syntax error'
 // var str2 = 'hello \'world\'' ;  ai vabe escape notation use kora jai tahole vitore text single ba double qoute ar modhe dekhano jabe
 var str2 = 'hello \'world\''
@@ -49,4 +51,28 @@ var str8 = 'hello\b world' ;
 // তারপর \f → ফর্ম ফিড, যেটা প্রিন্টার বা টার্মিনালে পেজ পরিবর্তনের মতো আচরণ করে
 // তারপর লেখা হবে world 
 var str9 = 'hello\fworld' ;
-console.log(str9) ;
+
+
+
+//** String Comparison */
+
+// Javascript use Lexicographic System to Compare to String
+// var a = 'baaaZ'
+// var b = 'aaaz'
+// console.log(a > b)
+
+//console.log('001' == 1) //aikahne '001' ke compare korar somoy number a convert kore compare kortese
+//console.log('001' === 1) //aikahne '001' ke compare korar somoy number a convert kore compare kortese na
+//console.log('001' !== 1) //aikahne '001' ke compare korar somoy number a convert kore compare kortese na
+
+/* ***Javascript Built in String Method*** */
+
+var a = 'I am'
+var b = "Ruhul Amin"
+var c = a.concat(' ',b) // concat method duita string ke jora lagai 
+
+var d = c.substring(4,6) // substring method ar madhome c variable 4 number character theke 8 number character projontto nissa
+//var d = c.substring(4) // ai vabe dila substring method ar madhome c variable 6 number character theke sesh  projontto nissa
+
+
+//console.log(c.charAt(5)) // ai chartAt method ar madhome koi character number dea oi character dekhte pari
