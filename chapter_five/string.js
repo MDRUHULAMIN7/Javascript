7d45732d12a908b5914b9b4bd505076ffc0c9451
Ruhul Amin <ruhulthisis@gmail.com>
2025-09-16 00:52:13 +0600

    Chapter five start

---
+++ chapter_five/string.js
@@ -0,0 +1,20 @@
+'ddddddddddd' ;
+"fdfesrsetgsrg" ;
+
+var str = 'hello world' ;
+var str2 = String('some')
+
+var n = 10;
+var str3 = n.toString() ;
+var str4 = String(n)
+var str5 =new String(n)
+console.log(str3,str4,str5) ;
+
+/* 
+    String methods
+
+    ai khane onek vabe string declare kora jai jemon 
+    var str = 'hello world' ; letaral string 
+    var str2 = 'hello world' ;
+    var str3 =  String('hello world') ; using String Construictor
+*/
