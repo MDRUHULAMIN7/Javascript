c1f127086b4607fe6bece7506396cff0cac77a21
Ruhul Amin <ruhulthisis@gmail.com>
2025-09-17 04:02:17 +0600

    starting object

---
+++ chapter_seven/object.js
@@ -0,0 +1,39 @@
+                    //*** Object  ***/
+
+//*** What is  Object ? ***/
+
+//Object is a collection of key value pairs.
+
+
+              // declare object by literal //
+var obj = {
+    // name: 'Ruhul',
+    // age: 25,
+    // city: 'Dhaka'
+}
+obj.x = 100
+console.log(obj) // object // {x: 100}
+
+
+var point = {
+    x: 100,
+    y: 200
+}
+point.z = 300
+console.log(point) // {x: 100, y: 200, z: 300}
+console.log(point.x); // access obj property by dot notation 
+console.log(point['x']); // access obj property by bracket notation
+
+
+          // declare object by object constructor //
+
+var obj2 = Object();
+obj2.a = 100;
+console.log(obj2) // {a: 100}
+
+
+var obj3 =new Object();
+obj3.a = 7100;
+console.log(obj3) // {a: 7100}
+
+
