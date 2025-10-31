c0b933d3fe81e46acfbac502bd9586e95db43af0
mdruhulamin7 <ruhulthisis@gmail.com>
2025-10-31 09:11:39 +0600

    scope

--- chapter_ten/function.js
+++ chapter_ten/function.js
@@ -5,3 +5,13 @@ function abc(){
 }
 
 abc()
+
+var newAbc = function(){
+    console.log(' i am new function abc')
+}
+newAbc()
+// Creational phase 
+// abc = ref 
+// newAbc = undefined 
+
+// here i s the example of different between  function declaration and expression
---
+++ chapter_ten/scope.js
@@ -0,0 +1,12 @@
+// javascript a seystem ta data gula variable  store kore thake segulake bole scope
+console.log(x)
+var x = 55
+
+function test(){
+// var x = 44
+console.log(x)
+}
+console.log(x)
+
+
+test()
